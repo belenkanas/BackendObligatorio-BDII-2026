bad8ed81e7e941b3fd15e6a5f02591091158b5b0
luletteiriarte <carolina.iriarte@correo.ucu.edu.uy>
2026-06-15 16:47:40 -0300

    agrego sector func

--- backend/src/main/java/com/obligatorio/backend/repository/FuncionarioAsignadoASectorRepository.java
+++ backend/src/main/java/com/obligatorio/backend/repository/FuncionarioAsignadoASectorRepository.java
@@ -2,11 +2,15 @@ package com.obligatorio.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 import com.obligatorio.backend.model.FuncionarioAsignadoASector;
 import com.obligatorio.backend.model.FuncionarioAsignadoASectorId;
 
 @Repository
+
 public interface FuncionarioAsignadoASectorRepository
         extends JpaRepository<FuncionarioAsignadoASector, FuncionarioAsignadoASectorId> {
+
+    List<FuncionarioAsignadoASector> findByIdNroLegajo(String nroLegajo);
 }
