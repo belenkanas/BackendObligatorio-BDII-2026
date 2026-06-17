57df3c8b1397fc7c0ff43e32d59ef4f9129c905a
belenkanas <maria.kanas@correo.ucu.edu.uy>
2026-06-17 18:36:30 -0300

    eliminacion correcta de usuarios

--- backend/src/main/java/com/obligatorio/backend/service/GeneralService.java
+++ backend/src/main/java/com/obligatorio/backend/service/GeneralService.java
@@ -5,9 +5,15 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.obligatorio.backend.model.General;
+import com.obligatorio.backend.model.Perfil;
+import com.obligatorio.backend.model.Usuario;
 import com.obligatorio.backend.repository.GeneralRepository;
+import com.obligatorio.backend.repository.PerfilRepository;
+import com.obligatorio.backend.repository.UsuarioRepository;
+
 
 @Service
 public class GeneralService {
@@ -15,19 +21,49 @@ public class GeneralService {
     @Autowired
     private GeneralRepository generalRepository;
 
+    @Autowired
+    private PerfilRepository perfilRepository;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+
     public List<General> obtenerTodos() {
         return generalRepository.findAll();
     }
+
 
     public Optional<General> obtenerPorId(Integer id) {
         return generalRepository.findById(id);
     }
 
+
     public General crear(General general) {
         return generalRepository.save(general);
     }
 
+
+    @Transactional
     public void eliminar(Integer id) {
-        generalRepository.deleteById(id);
+
+        General general = generalRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("General no encontrado"));
+
+
+        Perfil perfil = general.getPerfil();
+
+        Usuario usuario = perfil.getUsuario();
+
+
+        // eliminar general
+        generalRepository.delete(general);
+
+
+        // eliminar perfil
+        perfilRepository.delete(perfil);
+
+
+        // eliminar usuario
+        usuarioRepository.delete(usuario);
     }
 }
